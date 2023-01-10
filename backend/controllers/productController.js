b1f1a9aa050af27d602ff708e9ec7c5552206245
Chirag Ailani <chiragailani35@gmail.com>
2023-01-10 17:00:14 +0530

    Done Api Feature (Pagination...)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -13,7 +14,17 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors( async (req,res,next)=>{
-    const products = await Product.find();
+
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+    const apiFeature = new ApiFeatures(Product.find(),req.query)
+        .search()
+        .filter()
+        .pagination(resultPerPage);
+
+
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
         products
@@ -28,7 +39,8 @@ exports.getProductDetails = catchAsyncErrors( async (req,res,next) => {
     }
     res.status(200).json({
         success:true,
-        product
+        product,
+        productCount
     })
 });
 
