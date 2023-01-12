cbc75f05fd2022e78fa9b98cad8b13853b37d6b4
Priyanshu <80645068+PriyanshuQ@users.noreply.github.com>
2023-01-13 02:33:44 +0530

    minorchanges

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,8 @@ const connectDatabase = () => {
     mongoose.set('strictQuery', false);
     mongoose.connect(process.env.DB_URI).then((data)=>{
         console.log(`MongoDB connected with server: ${data.connection.host}`)
+    }).catch((err)=>{
+        console.log(err)
     })
 }
 
