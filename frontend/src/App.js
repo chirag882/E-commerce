cf23bca64fd424b7897249247498082829ba9273
Priyanshu <80645068+PriyanshuQ@users.noreply.github.com>
2023-01-13 14:06:31 +0530

    Search&loginsignup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Footer from "./component/layout/Footer/Footer.js"
 import Home from "./component/Home/Home.js"
 import Loader from './component/layout/Loader/Loader';
 import ProductDetails from "./component/Product/ProductDetails.js"
+import Products from "./component/Product/Products.js"
+import Search from "./component/Product/Search.js"
+import LoginSignUp from './component/User/LoginSignUp.js';
 
 
 function App() {
@@ -24,6 +27,10 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home />}/>
       <Route exact path="/product/:id" element={<ProductDetails />}/>
+      <Route exact path="/products" element={<Products />}/>
+      <Route path="/products/:keyword" element={<Products />}/>
+      <Route exact path="/search" element={<Search />}/>
+      <Route exact path="/login" element={<LoginSignUp />}/>
     </Routes>
     <Footer />
   </Router>
