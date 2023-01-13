615b8f6d7104d59ac52024aba1d26fda20314cbd
Chirag Ailani <chiragailani35@gmail.com>
2023-01-14 02:24:31 +0530

    Completed Backend Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,68 +1,141 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
+const sendEmail = require("../utils/sendEmail");
 const sendToken = require("../utils/jwtToken");
+const crypto = require("crypto");
 
 // Register User
-exports.registerUser = catchAsyncErrors( async (req,res,next)=>{
-    
-    const {name,email,password} = req.body;
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,email,password,
-        avatar:{
-            public_id : "This is a sample ID",
-            url : "hdnksmakfm"
-        }
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "This is a sample ID",
+      url: "hdnksmakfm",
+    },
+  });
 
-    sendToken(user,201,res);
-
+  sendToken(user, 201, res);
 });
 
 // Login User
 
-exports.loginUser = catchAsyncErrors( async (req,res,next) =>{
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    const {email,password} = req.body;
+  // checking if user has given password and email both
 
-    // checking if user has given password and email both
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 404));
+  }
 
-    if(!email || !password){
-        return next(new ErrorHandler("Please enter email and password",404));
-    }
+  const user = await User.findOne({ email }).select("+password");
+  // select has been used because in model we have mentioned password as {select : false}
 
-    const user = await User.findOne({email}).select("+password"); 
-    // select has been used because in model we have mentioned password as {select : false}
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password"));
+  }
 
-    if(!user){
-        return next(new ErrorHandler("Invalid email or password"));
-    }
+  const isPasswordMatched = await user.comparePassword(password);
 
-    const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password"));
+  }
 
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid email or password"));
-    }
+  const token = user.getJWTToken();
 
-    const token = user.getJWTToken();
-
-    sendToken(user,200,res);
-
-})
+  sendToken(user, 200, res);
+});
 
 // Logout User
 
-exports.logout = catchAsyncErrors( async (req,res,next)=>{
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-    res.cookie("token",null,{
-        expires : new Date(Date.now()),
-        httpOnly: true,
-    })
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
 
     res.status(200).json({
-        success : true,
-        message : "Logged Out"
-    })
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // creating token hash
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset Password Token is invalid or has been expired",
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not password", 400));
+  }
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
 });
