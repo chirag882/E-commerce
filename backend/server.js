d20b6b8d4154be0994ef3331de270e2512a5264f
Chirag Ailani <chiragailani35@gmail.com>
2023-01-24 14:03:48 +0530

    Create Order Bug Solved

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
 // handling uncaught exception
@@ -15,6 +16,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // connecting to database
 connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on localhost: ${process.env.PORT}`);
